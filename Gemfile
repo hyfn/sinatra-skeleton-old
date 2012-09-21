b717ea5b76cf17c5d2dced154c9bf1a73bc62301
Daniel Leavitt <daniel.leavitt@gmail.com>
2012-09-20 21:15:11 -0700

    additional persistence / server options

--- Gemfile
+++ Gemfile
@@ -2,22 +2,35 @@ source :rubygems
 
 gem "sinatra", :require => "sinatra/base"
 gem "sinatra-contrib"
+gem "rack-contrib"
 
 # Automated tasks
 gem "rake"
 
-# ORM (with MySQL)
-gem "data_mapper", "~> 1.2.0"
-gem "dm-postgres-adapter"
-
-# ... or if you want to use MySQL
+# DataMapper
+# gem "data_mapper", "~> 1.2.0"
+# with PG
+# gem "dm-postgres-adapter"
+# with MySQL
 # gem "dm-mysql-adapter"
+
+# Sequel
+# gem "sequel"
+# gem "mysql2"
+# gem "pg"
+
+# Redis
+# gem "redis"
+# gem "redis-namespace-with-multi", :require => "redis/namespace"
+# gem "redis-rack" # ability to user redis for session/cache
 
 # Console
 gem "racksh"
 
-# Web server
-gem "thin"
+# Server
+gem "unicorn"
+# gem "puma"
+# gem "thin"
 
 # Asset pipeline
 gem "sprockets", "~> 2.3.2"
@@ -25,5 +38,5 @@ gem "coffee-script", "~> 2.2.0"
 gem "sass"
 gem "coffee-filter"
 
-# Use HAML instead of ERB; its faster
-# gem "haml"
+# Use HAML instead of ERB
+gem "haml"
--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require "rubygems"
 require "bundler"
 Bundler.require
 
-DataMapper::Logger.new(STDOUT, :debug)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/app_development")
+# DataMapper
+#
+# DataMapper::Logger.new(STDOUT, :debug)
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/app_development")
+
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |model| require model }
 
 require './helpers'
@@ -15,21 +18,34 @@ class App < Sinatra::Base
   # sinatra-contrib's default protection:
   #
   # disable :protection
-
+  
+  # Sinatra uses some dumb default
+  set :json_encoder, :to_json
+  
+  # Sequel
+  #
+  # DB = Sequel.connect(ENV['DATABASE_URL'] || "mysql2://root:root@localhost/app_development")
+  # DB.loggers << Logger.new(STDOUT)
+  
+  # Redis
+  # 
+  # $redis = Redis::Namespace.new(:appname, :redis => Redis.new({
+  #   :url => ENV['REDIS_URL'], 
+  #   :logger => Logger.new(STDOUT)
+  # }))
+  
   set :root, File.dirname(__FILE__)
   set :sprockets, (Sprockets::Environment.new(root) { |env| env.logger = Logger.new(STDOUT) })
+  
   set :assets_path, File.join(root, 'assets')
-
-  configure do
-    # look for gems in asset folders
-    %w{javascripts stylesheets images}.each do |subdir|
-      sprockets.append_path File.join(root, 'assets', subdir)
-      %w{vendor lib app}.each do |base_dir|  
-        # load for all gems
-        Gem.loaded_specs.map(&:last).each do |gemspec|
-          path = File.join(gemspec.gem_dir, base_dir, "assets", subdir)
-          sprockets.append_path path if File.directory? path
-        end
+  # look for gems in asset folders
+  %w{javascripts stylesheets images}.each do |subdir|
+    sprockets.append_path File.join(root, 'assets', subdir)
+    %w{vendor lib app}.each do |base_dir|  
+      # load for all gems
+      Gem.loaded_specs.map(&:last).each do |gemspec|
+        path = File.join(gemspec.gem_dir, base_dir, "assets", subdir)
+        sprockets.append_path path if File.directory? path
       end
     end
   end
@@ -39,6 +55,9 @@ class App < Sinatra::Base
     also_reload "./**.rb"
   end
   
+  # rack middleware to parse json body
+  use Rack::PostBodyContentTypeParser
+  
   helpers Sinatra::AssetHelpers
 
   get "/" do
@@ -46,4 +65,6 @@ class App < Sinatra::Base
   end
 end
 
-DataMapper.finalize
+# more DataMapper
+#
+# DataMapper.finalize
